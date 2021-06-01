0d258fefafd404ca41edd42dbce85e48ac1225ea
Isaque F <isaquefrankli@gmail.com>
2021-06-01 20:14:18 -0300

    first commit

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import useFormValidation from "./useFormValidation";
+import validateLogin from './validateLogin'
+import firebase from '../../firebase'
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
+
+
+function Login(props) {
+
+  const { handleSubmit, handleChange, handleBlur, errors, isSubmitting, values } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser)
+  const [login, setLogin] = useState(true)
+
+  async function authenticateUser() {
+    const { name, email, password } = values
+    const response = login 
+      ? await firebase.login(email, password)
+      : await firebase.register(name, email, password)
+    console.log({ response })
+  }
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && (
+        <input 
+        onBlur={handleBlur}
+        onChange={handleChange} 
+        value={values.name}
+        type="text" 
+        placeholder="Your name" 
+        autoComplete="off" 
+        name="name" />
+        )}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          type="email"
+          placeholder="Your email"
+          autoComplete="off"
+          name="email"
+          className={errors.email && 'error-input'}
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          type="password"
+          placeholder="Choose a secure password"
+          name="password"
+          className={errors.password && 'error-input'}
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2" disabled={isSubmitting} style={{ background: isSubmitting ? "grey" : "orange"}}>
+            Submit
+          </button>
+          <button type="button" className="pointer button" onClick={() => setLogin(prevLogin => !prevLogin)}>
+            {login ? "need to create an account?" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBCCh4im8VMqeVCN1tWziFDSy2RceQ9dHo",
+    authDomain: "hacker-news-57514.firebaseapp.com",
+    projectId: "hacker-news-57514",
+    storageBucket: "hacker-news-57514.appspot.com",
+    messagingSenderId: "768260048408",
+    appId: "1:768260048408:web:d3df7d7dc90c19fb27a56d"
+};
+
+export default firebaseConfig;
